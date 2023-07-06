705819265fb0fe360e69c5596acf3cdb79ef7cda
Tang <lengsukq@foxmail.com>
2023-07-06 09:55:33 +0800

    白嫖机场终极版

--- 白嫖机场.py
+++ 白嫖机场.py
@@ -1,4 +1,6 @@
 # 白嫖机场自动订阅
+import os
+import subprocess
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
@@ -6,21 +8,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def get_edge_action():
+def get_edge_action(num=2):
+    # 先切换到chrome可执行文件的路径
+    # os.chdir(r"C:\Program Files (x86)\Microsoft\Edge\Application")
+    # user-data-dir为路径
+    # subprocess.Popen('msedge.exe --remote-debugging-port=9999 --user-data-dir="D:\AutomationProfile"')
     options = webdriver.EdgeOptions()  # 自定义加载项
     options.add_experimental_option("debuggerAddress", "127.0.0.1:9999")  # 本地监听edge浏览器
     browser = webdriver.Edge(options=options)
     url = "https://xn--mesv7f5toqlp.com/#/plan/1"
-    browser.get(url)
-    sleep(3)
-    browser.find_element(By.XPATH, '/html/body/div[1]/div/main/div/div/div[2]/div[1]/input').send_keys('baipiao9356')
-    browser.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[2]/div[1]/button').click()
-    sleep(3)
-    browser.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[2]/div[2]/button').click()
-    sleep(5)
-    browser.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[2]/div/button').click()
+    nowNum = 0
+
+    while nowNum < num:
+        browser.get(url)
+        sleep(8)
+        browser.find_element(By.XPATH, '/html/body/div[1]/div/main/div/div/div[2]/div[1]/input').send_keys(
+            'baipiao9356')
+        browser.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[2]/div[1]/button').click()
+        sleep(8)
+        browser.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[2]/div[2]/button').click()
+        sleep(8)
+        browser.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[2]/div/button').click()
+        sleep(8)
+        nowNum = nowNum + 1
+
 
 if __name__ == '__main__':
     print('开始执行')
-    get_edge_action()
+    get_edge_action(24)
     print('结束')
